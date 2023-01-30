bf4302f427153b17054883bf04ddfdff8632db5d
Mae <stringy@mae.wtf>
2023-01-30 22:31:55 +0000

    remove ssr for now :/

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -3,15 +3,22 @@ import Image from 'next/image'
 import Link from 'next/link'
 
 import axios from 'axios'
+import { useQuery } from 'react-query';
+import LoadingSpinner from '@oc/components/LoadingSpinner';
 
-Home.getInitialProps = async () => {
-  const data = await axios.get(process.env.NEXT_DOMAIN + '/api/frontpage')
-  return {
-    pages: data.data
-  }
-}
+// Index.getInitialProps = async () => {
+//   const data = await axios.get(process.env.NEXT_DOMAIN + '/api/frontpage')
+//   return {
+//     pages: data.data
+//   }
+// }
 
-export default function Home(props: {pages: any;}) {
+export default function Index(props: {pages: any;}) {
+  const pages = useQuery("frontpage", async () => {
+    const data = await axios.get('/api/frontpage')
+    return data.data
+  })  
+  
   return (
     <div className='flex items-center flex-col pt-12'>
       <div className='flex items-center space-x-8 py-6  '>
@@ -20,7 +27,9 @@ export default function Home(props: {pages: any;}) {
       </div>
       <div className='py-8 flex-col items-center grid grid-cols-3 gap-4'>
         {
-          props.pages.map((p: any) => (
+          (pages.isLoading)
+          ? <LoadingSpinner fill="#fff" className='w-12'></LoadingSpinner>
+          : pages.data.map((p: any) => (
             <a href={'https://' + p.domain}>
               <div className="flex items-center justify-center h-40 w-40 rounded-md text-3xl shadow-lg hover:-translate-y-1 transition-all duration-75" style={{backgroundColor: '#' + p.color}}>
                 <h1>{p.name}</h1>
